b1a5d4b587ae87aa36bf8a925fe2dc82c02a53cb
Sumii <64231313+PejperO@users.noreply.github.com>
2023-04-04 22:44:20 +0200

    Update prime_numbers.py

--- src/prime_numbers.py
+++ src/prime_numbers.py
@@ -1,19 +1,28 @@
-number = int(input("Enter the number you want to check if it is a prime number: "))
+numbers = input("Enter the numbers you want to check if they are prime numbers (separated by commas): ")
+numbers_list = numbers.split(",")
 
 
-def is_prime():
-    if number <= 1:
-        return "not prime"
-    elif number <= 3:
-        return "prime"
-    elif number % 2 == 0 or number % 3 == 0:
-        return "not prime"
-    i = 5
-    while i * i <= number:
-        if number % i == 0 or number % (i + 2) == 0:
-            return "not prime"
-        i += 6
-    return "prime"
+def are_primes(*args):
+    def is_prime(number):
+        number = int(number)
+        if number <= 1:
+            return False
+        elif number <= 3:
+            return True
+        elif number % 2 == 0 or number % 3 == 0:
+            return False
+        i = 5
+        while i * i <= number:
+            if number % i == 0 or number % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+    for n in args:
+        if is_prime(int(n)):
+            print(f"{n} is prime")
+        else:
+            print(f"{n} is not prime")
 
 
-print("Number " + str(number) + " is " + is_prime())
+are_primes(*numbers_list)
